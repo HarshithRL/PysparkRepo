d618c10eabc8e0859e5b4daf187cb458ee52a45a
HarshithRL <harshith.r@diggibyte.com>
2023-09-13 23:04:25 +0530

    completed coding of 1st assinment

--- src/assignment_1/driver.py
+++ src/assignment_1/driver.py
@@ -1,4 +1,16 @@
 from PysparkRepo.src.assignment_1.utils import  *
+
+
+Schema=["Product Name","Issue Date","Price","Brand","Country","Product number"]
+dataStruct = [('Washing Machine',1648770933000,20000,'Samsung','India',1),
+      ('Refrigerator',1648770999000,35000,' LG',None,2),
+      ('Air Cooler',1648770948000,45000,'Voltas ',None,3)]
+#df = sc.createDataFrame(dataStruct,Schema)
+
+colName1="Issue Date"
+colName="Country"
+ColName2="Brand"
+
 df=create_df(sc,dataStruct,Schema)
 
 #Convert the Issue Date with the timestamp format
--- src/assignment_1/utils.py
+++ src/assignment_1/utils.py
@@ -2,17 +2,6 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_unixtime, to_date, trim
 sc=SparkSession.builder.appName('name').getOrCreate()
 
-Schema=["Product Name","Issue Date","Price","Brand","Country","Product number"]
-dataStruct = [('Washing Machine',1648770933000,20000,'Samsung','India',1),
-      ('Refrigerator',1648770999000,35000,' LG',None,2),
-      ('Air Cooler',1648770948000,45000,'Voltas ',None,3)]
-#df = sc.createDataFrame(dataStruct,Schema)
-
-colName1="Issue Date"
-colName="Country"
-ColName2="Brand"
-
-
 def create_session():
     return SparkSession.builder.appName('name').getOrCreate()
 def create_df(sc,data,schema):
