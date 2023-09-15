5e9dd249f6a809a5a2de94455e6217d62191b3ea
HarshithRL <harshith.r@diggibyte.com>
2023-09-15 20:25:10 +0530

    solved 2nd part of 1st assignment

--- src/assignment_1/driver.py
+++ src/assignment_1/driver.py
@@ -1,10 +1,27 @@
 from PysparkRepo.src.assignment_1.utils import  *
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, TimestampType
 
 
 Schema=["Product Name","Issue Date","Price","Brand","Country","Product number"]
 dataStruct = [('Washing Machine',1648770933000,20000,'Samsung','India',1),
       ('Refrigerator',1648770999000,35000,' LG',None,2),
       ('Air Cooler',1648770948000,45000,'Voltas ',None,3)]
+schema_1 = StructType([
+    StructField("SourceId", IntegerType(), True),
+    StructField("TransactionNumber", IntegerType(), True),
+    StructField("Language", StringType(), True),
+    StructField("ModelNumber", IntegerType(), True),
+    StructField("StartTime", StringType(), True),
+    StructField("Product number", IntegerType(), True)
+])
+
+# Define the data for dataStruct_1
+dataStruct_1 = [
+    (150711, 123456, 'EN', 456789, '2021-12-27T08:20:29.842+0000', 1),
+    (150439, 234567, 'UK', 345678, '2021-12-27T08:21:14.645+0000', 2),
+    (150647, 345678, 'ES', 234567, '2021-12-27T08:22:42.445+0000', 3)
+]
+
 
 colName1="Issue Date"
 colName="Country"
@@ -13,24 +30,43 @@ ColName2="Brand"
 #start Session
 sc=create_session()
 
+"""Question 1"""
 #create Data Frame
-df=create_df(sc,dataStruct,Schema)
+df_product_1=create_df(sc,dataStruct,Schema)
+
 
 #Convert the Issue Date with the timestamp format
-df=format_date(df,colName1)
-df.show()
+df_formate_timestamp_product_1=format_timestamp(df_product_1,colName1)
+df_formate_timestamp_product_1.show()
 
 #Convert timestamp to date type
-df=format_type(df,colName1)
-df.show()
+df_formate_date_product_1=format_type(df_formate_timestamp_product_1,colName1)
+df_formate_date_product_1.show()
 
 #Remove the starting extra space in Brand column for LG and Voltas fields
-df=trim_str(df,ColName2)
-df.show()
+df_space_trim_product_1=trim_str(df_formate_date_product_1,ColName2)
+df_space_trim_product_1.show()
 
 #Replace null values with empty values in Country column
-df=fill_na(df,colName)
-df.show()
+df_replaced_null_product_1=fill_na(df_space_trim_product_1,colName)
+df_replaced_null_product_1.show()
+
+"""Question 2"""
+
+#create Data Frame
+df_product_2=create_df(sc,dataStruct_1,schema_1)
+
+df_renamed=rename_column(df_product_2,'SourceId','source_id')
+
+df_renamed=rename_column(df_renamed,'TransactionNumber','transaction_number')
+
+#formate from timestamp to unix
+df_format_unix=format_unix(df_renamed,'StartTime')
+df_format_unix.show()
+
+#Join the table
+joined_table=join_table(df_replaced_null_product_1,df_product_2,"Product number","Product number")
+joined_table.show()
 
 #stop Session
 sc.stop()
--- src/assignment_1/utils.py
+++ src/assignment_1/utils.py
@@ -1,29 +1,50 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_unixtime, to_date, trim
-
+from pyspark.sql.functions import from_unixtime, to_date, trim,unix_timestamp,col
+from pyspark.sql.types import TimestampType , DoubleType
 
 #Function to create Session
 def create_session():
     return SparkSession.builder.appName('name').getOrCreate()
 
-#function to create Data frame
+#Function to create Data frame
 def create_df(sc,data,schema):
     return sc.createDataFrame(data,schema)
 
-#function to Formate the date & time
-def format_date(df,colName1):
+
+#Function to Formate the date & time
+def format_timestamp(df,colName1):
     return df.withColumn(colName1, from_unixtime(df[colName1] / 1000).cast("timestamp"))
 
-#function to Change Data type
+
+
+#Function to Change Data type
 def format_type(df,colName1):
     return df.withColumn(colName1, to_date(df[colName1]))
 
-#function to trim the Space
+
+#Function to trim the Space
 def trim_str(df,ColName2):
     return df.withColumn(ColName2, trim(df[ColName2]))
 
-#function to replace null value
+
+#Function to replace null value
 def fill_na(df,colName):
     return df.na.fill("", subset=[colName])
 
 
+#question 2
+
+#Function to rename column
+def rename_column(df,existingName,newNam):
+    return df.withColumnRenamed(existingName,newNam)
+
+
+#Function to convert to unix
+def format_unix(df,colName1):
+    timestamp_format = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
+    return df.withColumn('timestamp_format', unix_timestamp(df[colName1], timestamp_format).cast(DoubleType()))
+
+
+#joining Table
+def join_table(df,df2,colName,colName1):
+    return df.join(df2,df[colName]==df2[colName1],"outer")
